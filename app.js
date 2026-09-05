b6b6f427815810e1947391e5b1be4832f386254c
mahathiss <mahathi22027@gmail.com>
2026-09-06 01:10:07 +0530

    Remove API key from CartoDB public tiles to fix watermark error

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ async function initMap() {
   }
 
   // Base Tiles: CartoDB Positron
-  L.tileLayer(`https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png${cartoKey}`, {
+  L.tileLayer(`https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png`, {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
     maxZoom: 19,
     subdomains: "abcd"
