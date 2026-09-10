279e1c08217d4529f25881f807b443009eb85919
Nishtha Patel <u25cs061@coed.svnit.ac.in>
2026-09-10 15:29:12 +0530

    Fix Carto API key error - switch to OpenStreetMap tiles

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ function initMap() {
   });
 
   // Base Tiles: CartoDB Positron
-  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 19
   }).addTo(window.floodMap);
 
